89356ee9b1838ebde9d432f9a2a422fd0a5e30e8
pudvik <prudviksanikala54@gmail.com>
2024-04-16 12:15:52 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=ravi
+PERSON2=ram
+
+echo "$PERSON1::Hi $PERSON2,How are you?"
+echo "$PERSON2::Hi $PERSON1, i am fine ,how r u?"
+echo "$PERSON1::i am fine , wt r u dng?"
+echo "$PERSON2::i am learning devops"

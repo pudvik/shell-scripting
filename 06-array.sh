4eadfd509bbf5e8ec52b6ec17f699caa7aca7a3d
pudvik <prudviksanikala54@gmail.com>
2024-04-16 13:53:43 +0530

    variables

---
+++ 06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+movies=("RRR" "PUSHPA" "KING")
+
+#ARRAYSIIZE=3
+#INDEX STARTS WITH 0
+
+echo "first movie is: ${movies[0]}"
+echo "first movie is: ${movies[1]}"
+echo "first movie is: ${movies[@]}"

5bbe20a20d48043bf510639d8373ab68a280ae08
pudvik <prudviksanikala54@gmail.com>
2024-04-23 15:13:52 +0530

    shell-script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,6 +5,6 @@ VAR="Learning DevOps"
 echo "Before calling other script , var : $VAR"
 echo "process id of current shell script : $$"
 
-./16-other-script.share
+./16-other-script.sh
 
 echo "After calling shell script , var : $VAR"

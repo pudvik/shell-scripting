23bc31717590fee819a3c2fe6813548af91a1d03
pudvik <prudviksanikala54@gmail.com>
2024-04-25 10:13:37 +0530

    shell-script

--- a1-read-data.sh
+++ a1-read-data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-file-path=/home/ec2-user
+file-path=/home/ec2-user/abc
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-while IFS = read -r line 
+while IFS= read -r line 
 do 
     echo "a line : $line"
 
